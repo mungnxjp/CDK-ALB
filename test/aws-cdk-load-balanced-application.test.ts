e1246eb174b469f66352cbcd6d0ed713dc1b5cd3
Nguyen Xuan Mung (VTI) <m_nguyen@ga-tech.co.jp>
2024-03-23 15:15:57 +0900

    Initial commit

---
+++ test/aws-cdk-load-balanced-application.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkLoadBalancedApplication from '../lib/aws-cdk-load-balanced-application-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-load-balanced-application-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkLoadBalancedApplication.AwsCdkLoadBalancedApplicationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
